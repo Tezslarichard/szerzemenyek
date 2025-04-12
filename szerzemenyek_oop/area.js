4e71c52efe7b717df56d64eb3d1b67cd5af9438c
Tezslarichard <tezslarichard@gmail.com>
2025-04-12 14:30:48 +0200

    tablazat letrehozasa oop

--- szerzemenyek_oop/area.js
+++ szerzemenyek_oop/area.js
@@ -1,5 +1,12 @@
 //area class megcsinalom
 class Area{
+
+    #div //csinalok egy privat valtozot
+
+    get div(){// letrehozok egy gettert ami visszaadja a div elemet
+        return this.#div// visszaadom a div elemet
+    }
+
     /**
      * 
      * @param {string} osztaly majd a vegen irom be
@@ -12,8 +19,32 @@ class Area{
             document.body.appendChild(d_container)// hozzadom a bodyhoz
         }
 
-        const div = document.createElement("div")// letrehozok egy div elemet
-        div.className = osztaly// osztalynevet adok neki
-        d_container.appendChild(div)// hozzadom a containeroop divhez
+        this.#div = document.createElement("div")// letrehozok egy div elemet
+        this.#div.className = osztaly// osztalynevet adok neki
+        d_container.appendChild(this.#div)// hozzadom a containeroop divhez
+    }
+}
+
+/**
+ * 
+ */
+class Table extends Area{// letrehozok egy Table osztalyt ami az Area osztalybol szarmazik
+    constructor(cssClass){// letrehozok egy constructort es parameterbe megadok valamit
+        super(cssClass)// meghivom a szulo osztaly konstruktorat
+        const table = document.createElement("table")// letrehozok egy table elemet
+        this.div.appendChild(table)// hozzadom a divhez
+        const thead = document.createElement("thead")// letrehozok egy thead elemet
+        table.appendChild(thead)// hozzadom a tablehez
+        const th_row = document.createElement("tr")// letrehozok egy tr elemet
+        thead.appendChild(th_row)// hozzadom a theadhez
+        const th_ertek = ['Szerző', 'Műfaj','cím'] // eltarolom a fejlec adatait egy valtozoba
+        for(const fej of th_ertek){// vegigmegyek a th_ertek tomb elemein
+            const th = document.createElement("th")// letrehozok egy th elemet
+            th.innerText = fej// beallitom a szoveget
+            th_row.appendChild(th)// hozzadom a sorhoz
+        }
+        const tbody = document.createElement("tbody")// letrehozok egy tbody elemet
+        table.appendChild(tbody)// hozzadom a tablehez
+
     }
 }
--- szerzemenyek_oop/main.js
+++ szerzemenyek_oop/main.js
@@ -1,5 +1,5 @@
 const elvalaszto = document.createElement("hr")// letrehozok egy elvalaszto elemet
 document.body.appendChild(elvalaszto)// hozzadom a bodyhoz
 
-const tableoop = new Area('table')//letrehozok egy table  area peldanyt
+const tableoop = new Table('table')//letrehozok egy table peldanyt
 const formoop = new Area('form')//letrehozok egy form area peldanyt
